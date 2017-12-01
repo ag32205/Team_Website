d8eec1201909fc0fcad557a271d7a88b072a073e
Max W Lobato <ml93117@srcs.loc>
2017-12-01 11:22:30 -0800

    FINALLY WORKING BIRTHDATE

--- team_website_funtions.js
+++ team_website_funtions.js
@@ -1,1 +1,28 @@
+    function birthdayCountDown(bdate) {
+
+        var today = new Date();
+        var bday = new Date(bdate);
+        var currenttime = today.getTime();
+        var btime = bday.getTime();
+
+        if ((btime - currenttime) < 0) {
+
+
+            bday.setFullYear(today.getFullYear());
+
+            if ((bday.getTime() - today.getTime()) < 0) {
+                bday.setFullYear(bday.getFullYear() + 1);
+            }
+
+        }
+
+        var daysuntil = Math.floor((bday.getTime() / 86400000) - (today.getTime() / 86400000));
+
+
+        document.write("There are ", daysuntil, " days until my next birthday.");
+
+
+    }
+
+
 
