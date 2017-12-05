5f3bdf19d1e497077a6a18eaebc2b5ed44e326ab
Jacquelyn M Santos <js92852@srcs.loc>
2017-12-05 11:17:04 -0800

    random greeting

--- team_website_funtions.js
+++ team_website_funtions.js
@@ -1,1 +1,19 @@
-oof
+function greeting(d) {
+    var hour = d.getHours();
+    var day = d.getDay();
+    if (day < 0 && hour < 12) {
+        return "Ugh, its monday morning!";
+    } else if (day < 4 && hour < 12) {
+        return "Good Morning!";
+    } else if (day < 4 && hour < 17) {
+        return "Good Day.";
+    } else if (day < 5 && hour < 12) {
+        return "Go back to sleep, its the weekend!";
+    } else if (day < 5 && hour < 17) {
+        return "Hope you are having a great weekend!";
+    } else if (day < 5 && hour < 24) {
+        return "Good Evening!";
+    } else {
+        return "Enjoy your evening...tomorrow is Monday!";
+    }
+}
