94864cc54a4864faae2665e63110be034c6af0f9
Jacquelyn M Santos <js92852@srcs.loc>
2017-12-07 11:40:17 -0800

    corrected the greeting function

--- team_website_funtions.js
+++ team_website_funtions.js
@@ -1,20 +1,20 @@
-function greeting(d) {
-    var hour = d.getHours();
-    var day = d.getDay();
-    if (day < 0 && hour < 12) {
-        return "Ugh, its monday morning!";
-    } else if (day < 4 && hour < 12) {
-        return "Good Morning!";
-    } else if (day < 4 && hour < 17) {
-        return "Good Day.";
-    } else if (day < 5 && hour < 12) {
+function greeting(today) {
+    var hour = today.getHours();
+    var day = today.getDay();
+    if (day == 0 || day == 6 && hour < 12) {
         return "Go back to sleep, its the weekend!";
-    } else if (day < 5 && hour < 17) {
+    } else if (day == 0 || day == 6 && hour < 17) {
         return "Hope you are having a great weekend!";
-    } else if (day < 5 && hour < 24) {
-        return "Good Evening!";
-    } else {
+    } else if (day == 0 && hour < 24) {
         return "Enjoy your evening...tomorrow is Monday!";
+    } else if (day == 1 && hour < 12) {
+        return "Ugh, its monday morning!";
+    } else if (day > 1 && hour < 12) {
+        return "Good Morning!";
+    } else if (day == 5 || day < 5 && hour < 17) {
+        return "Good Day!";
+    } else {
+        return "Good Evening!";
     }
 }
 
